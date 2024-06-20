320af95f84acd82f5bad585bc78fbc7c0193ad14
Auzan <istikhoriauzan@gmail.com>
2024-06-20 14:13:18 +0700

    feat: login register endpoint

--- src/v1/controllers/account.ts
+++ src/v1/controllers/account.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import { db } from '../../utils/db';
 import { sendApiResponse } from '../../utils/api-response';
@@ -35,3 +37,60 @@ export const getAccountById = async (req: Request, res: Response): Promise<void>
     sendApiResponse(res, 'error', 500, null, 'Internal Server Error!');
   }
 };
+
+export const registerAccount = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await db.account.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+    
+    sendApiResponse(res, 'success', 201, null, 'Akun berhasil dibuat!');
+
+  } catch (error) {
+    console.log(error.message);
+    sendApiResponse(res, 'error', 500, null, 'Internal Server Error!');
+  }
+}
+
+export const loginAccount = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const data = await db.account.findUnique({
+      where: { email },
+    });
+
+    if (!data) {
+      return sendApiResponse(res, 'error', 404, null, 'Email tidak terdaftar!');
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, data.password);
+    if (!isPasswordMatch) {
+      return sendApiResponse(res, 'error', 400, null, 'Password salah!');
+    }
+
+    const secret = process.env.JWT_SECRET!;
+    const expiresIn = 60 * 60 * 24
+    const token = jwt.sign({ id: data.id }, secret, { expiresIn });
+
+    const payload = {
+      id: data.id,
+      email: data.email,
+      token,
+      expiresIn,
+    }
+
+    sendApiResponse(res, 'success', 200, payload, 'Login berhasil!');
+
+  } catch (error) {
+    console.log(error.message);
+    sendApiResponse(res, 'error', 500, null, 'Internal Server Error!');
+  }
+}
--- src/v1/routes/account.ts
+++ src/v1/routes/account.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 
-import { getAllAccount, getAccountById } from '../controllers/account';
+import { 
+    getAllAccount, 
+    getAccountById, 
+    loginAccount, 
+    registerAccount 
+} from '../controllers/account';
 
 const route = express.Router();
 
 route.get('/api/v1/account', getAllAccount);
 route.get('/api/v1/account/:id', getAccountById);
+route.post('/api/v1/register', registerAccount);
+route.post('/api/v1/login', loginAccount);
 
 export default route;
